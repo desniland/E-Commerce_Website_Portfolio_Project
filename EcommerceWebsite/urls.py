a35435c101e7055fe0e1d189ab2dda6a54720c2f
desniland <albertdesmond26@gmail.com>
2024-10-11 12:59:07 +0300

    URL configuration module

--- EcommerceWebsite/urls.py
+++ EcommerceWebsite/urls.py
@@ -1,22 +1,38 @@
-"""
-URL configuration for EcommerceWebsite project.
+#URL conf module
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.urls import path,include
+from .views import(
+ HomeView,
+ ProductDetailView,
+ add_to_cart,
+ remove_from_cart,
+ OrderSummaryView
+)
+
+
+from .import views
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
+
+app_name = "EcommerceWebsite"
+
+#URL configurations
 urlpatterns = [
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='index'),
+    path('product/<slug>/',ProductDetailView.as_view(), name='detail'),
+    path('add-to-cart/<slug>/',views.add_to_cart,name='add-to-cart'),
+    path('remove-from-cart/<slug>/',views.remove_from_cart,name='remove-from-cart'),
+    path('link/',views.detailitem,name='linkage'),
+    path('cart/', OrderSummaryView.as_view(),name='cart'),
+    path('checkout/',views.checkout, name='checkout'),
+    path('complete/',views.complete,name='complete'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
